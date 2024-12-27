92fa5dfbec458bb3718b96a5c89d4cbe8885e82b
vishesh-sachan <visheshsachan021@gmail.com>
2024-12-27 11:42:31 +0530

    updated readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+'use client'
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div>
-    hello
-  </div>
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (!session) {
+    return <p>You are not signed in. Please sign in to access this page.</p>;
+  }
+
+  return (
+    <div>
+      <h1>Welcome, {session.user.name}</h1>
+      <p>Your email: {session.user.email}</p>
+      <p>Your ID: {session.user.id}</p>
+      <p>Your Role: {session.user.role}</p>
+    </div>
+  );
 }
